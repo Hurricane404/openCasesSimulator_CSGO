a8e9121526b25841f94417dcadea35ebb4f28e38
PassaUmDollar <46573030+PassaUmDollar@users.noreply.github.com>
2019-10-07 15:22:41 -0300

    Fix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const app = new Vue({
+var app = new Vue({
     el: "#app",
     data: {
         items: [],
@@ -173,7 +173,7 @@ const app = new Vue({
                 localStorage.saldo = JSON.stringify(this.saldo)
                 localStorage.rank = JSON.stringify(this.rank)
                 setTimeout(() => this.open = false, 2000);
-                setTimeout(() => this.tryAgain = false, 500);
+                setTimeout(() => this.tryAgain = false, 2000);
             }
         },
         tryAgainOk() {
